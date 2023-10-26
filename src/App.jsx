5d20ec1fd74034033f5d18880cf0cc1ca1194174
Akash Kottil <kottilakash@gmail.com>
2023-10-26 21:28:44 +0530

    First Commmit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+import './App.css'
+import Home from './Components/Home/Home'
+export default App
